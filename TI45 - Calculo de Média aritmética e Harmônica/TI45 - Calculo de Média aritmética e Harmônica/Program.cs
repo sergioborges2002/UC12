736a4b694bd8d66e97e7a04b0c2017346b01a4db
sergiohbsantos201 <sergio.borges2002@gmail.com>
2024-11-04 09:04:39 -0300

    changes

---
+++ TI45 - Calculo de Média aritmética e Harmônica/TI45 - Calculo de Média aritmética e Harmônica/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TI45_ex7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, d, e, ma, mh;
+            Console.WriteLine("Média Aritmética e harmônica de 5 valores");
+            Console.WriteLine("Digite o 1º valor:");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o 2º valor:");
+            b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o 3º valor:");
+            c = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o 4º valor:");
+            d = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o 5º valor:");
+            e = double.Parse(Console.ReadLine());
+            ma = (a + b + c + d + e) / 5;
+            mh = 5 / (1 / a + 1 / b + 1 / c + 1 / d + 1 / e);
+            Console.WriteLine("Média Aritmética: " + Math.Round(ma, 4));
+            Console.WriteLine("Média Harmônica: " + Math.Round(ma, 4));
+            Console.ReadKey();
+        }
+    }
+}
