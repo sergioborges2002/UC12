9101536de04c9789edef5d7f4f4d98f5f747f546
sergiohbsantos201 <sergio.borges2002@gmail.com>
2024-11-04 10:31:28 -0300

    changes

---
+++ TI45 - Estrutura de Múltipla escolha/TI45 - Estrutura de Múltipla escolha/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TI45___Estrutura_de_Múltipla_escolha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int op;
+            Console.WriteLine("Estrutura de Múltipla escolha");
+            Console.WriteLine("Digite 1 ou 2");
+            op=int.Parse(Console.ReadLine());
+            switch (op){
+                case 1:
+                    Console.WriteLine("VB/Delphi");
+                    break;
+                    case 2:
+                    Console.WriteLine("C/C++/C#,Java");
+                    break;
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ TI45-Mês do ano/TI45-Mês do ano/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TI45_Mês_do_ano
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int mes;
+            Console.WriteLine("Mês do Ano - Númerico - Texto");
+            Console.WriteLine("Digite um mês do ano (valores entre 1 e 12): ");
+            mes = int.Parse(Console.ReadLine());
+            switch (mes)
+            {
+                case 1:
+                    Console.WriteLine("Janeiro");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Fevereiro");
+                    break;
+                case 3:
+                    Console.WriteLine("Março");
+                    break;
+                case 4:
+                    Console.WriteLine("Abril");
+                    break;
+                case 5:
+                    Console.WriteLine("Maio");
+                    break;
+                case 6:
+                    Console.WriteLine("Junho");
+                    break;
+                case 7:
+                    Console.WriteLine("Julho");
+                    break;
+
+                case 8:
+                    Console.WriteLine("Agosto");
+                    break;
+                case 9:
+                    Console.WriteLine("Setembro");
+                    break;
+                case 10:
+                    Console.WriteLine("Outubro");
+                    break;
+                case 11:
+                    Console.WriteLine("Novembro");
+                    break;
+                case 12:
+                    Console.WriteLine("Dezembro");
+
+                    break;
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
