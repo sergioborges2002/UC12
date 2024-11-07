7c25933989d07c8b4a7ee1158d8799d7597e7067
sergiohbsantos201 <sergio.borges2002@gmail.com>
2024-11-07 11:30:13 -0300

    changes

---
+++ TI45-Contadores de divisores/TI45-Contadores de divisores/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TI45_Contadores_de_divisores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n,cont=0;
+            Console.WriteLine("1 número positivo e seus divisores");
+            Console.WriteLine("Digite 1 nº positivo");
+            n=int.Parse(Console.ReadLine());
+            Console.WriteLine("Divisores de "+n+":");
+            Console.WriteLine();
+            for (i = 1; i <= n; i++)
+            {
+                if (n%i == 0)
+                {
+                    Console.Write(i+" ");
+                    cont++;
+                }
+            }
+            Console.WriteLine("\n\n"+n+" tem "+cont+" divisores.");
+            Console.ReadKey();
+        }
+    }
+}
